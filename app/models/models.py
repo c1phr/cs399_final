e6c882e492b0e1ca7be6ef2a485677a31d294709
Salvatore Bottiglieri <sb797@nau.edu>
2015-04-18 10:15:28 -0700

    models are created

--- app/models/models.py
+++ app/models/models.py
@@ -2,7 +2,26 @@ import os
 import webapp2
 from google.appengine.ext import ndb
 
-class UserInformation(ndb.Model):
-    first_name = ndb.StringProperty()
-    last_name = ndb.StringProperty()
-    email = ndb.StringProperty()
+class User(ndb.Model):
+    user_id = ndb.IntegerProperty(required=True)
+    first_name = ndb.StringProperty(required=True)
+    last_name = ndb.StringProperty(required=True)
+    gravatar_url = ndb.StringProperty()
+
+class Requirements(ndb.Model):
+    req_id = ndb.IntegerProperty(required=True)
+    parent_id = ndb.IntegerProperty()
+    req_desc = ndb.TextProperty()
+    project_id = ndb.IntegerProperty()
+
+class Project(ndb.Model):
+    project_id = ndb.IntegerProperty(required=True)
+    project_title = ndb.StringProperty()
+    project_desc = ndb.TextProperty()
+
+class Task(ndb.Model):
+    task_id = ndb.IntegerProperty(required=True)
+    task_title = ndb.StringProperty()
+    req_id = ndb.IntegerProperty()
+    assignee_id = ndb.IntegerProperty()
+    field = ndb.StringProperty()
