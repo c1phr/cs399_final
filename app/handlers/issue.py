fc1e336c3d11d314fb21c34c2f3cde608b2d293b
Duke Ayers <Dfa24@nau.edu>
2015-04-19 15:41:05 -0700

    Added "Closed" Issues
    Made a table and now are filtering between closed/open issues

--- app/handlers/issue.py
+++ app/handlers/issue.py
@@ -22,7 +22,17 @@ class Home(BaseHandler):
                                 headers={"Accept": "application/json"},
                                 deadline=10)
         open_issues_content = json.loads(result.content)
-        self.response.write(template.render(name="Issues",open_issues = open_issues_content,
+
+        #closed issues
+        closed_issues_url = "https://api.github.com/repos/" + self.session.get(
+            "username") + "/" + cgi.escape(id) + "/issues?access_token=" + self.session.get(
+            "access_token") + "&state=closed"
+        result = urlfetch.fetch(url=closed_issues_url,
+                                method=urlfetch.GET,
+                                headers={"Accept": "application/json"},
+                                deadline=10)
+        closed_issues_content = json.loads(result.content)
+        self.response.write(template.render(name="Issues",open_issues = open_issues_content,closed_issues = closed_issues_content,
                                             user=User.query(User.key == self.session.get("user")).get(), project = cgi.escape(id)))
 
 
