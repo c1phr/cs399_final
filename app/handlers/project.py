6aadf30bad87249d725209f84b1b7cf49cb5d38a
Ryan Batchelder <rb688@nau.edu>
2015-04-18 18:25:02 -0700

    Adding project framework

--- app/handlers/project.py
+++ app/handlers/project.py
@@ -1,7 +1,10 @@
 import os
 import webapp2
 from app.handlers.BaseHandler import BaseHandler
-from app.models.models import User
+from app.models.models import User, Project
 from jinja2 import Environment, PackageLoader
 env = Environment(loader=PackageLoader('app', 'templates'))
 
+class Project(BaseHandler):
+    def get(self, project_id):
+        project_data = Project.query(project_id == project_id)
