adea4f04bedde2623e3eab968d45d1956a10383e
Salvatore Bottiglieri <sb797@nau.edu>
2015-04-19 18:00:21 -0700

    five latest commits are now showing

--- app/handlers/dashboard.py
+++ app/handlers/dashboard.py
@@ -7,6 +7,7 @@ from secrets import secrets
 from app.handlers.BaseHandler import BaseHandler
 from app.models.models import User, Project, Project_User
 from jinja2 import Environment, PackageLoader
+from datetime import datetime
 env = Environment(loader=PackageLoader('app', 'templates'))
 
 
@@ -20,17 +21,48 @@ class MainHandler(BaseHandler):
                                 method=urlfetch.GET,
                                 headers={"Accept": "application/json"},
                                 deadline=10)
-        # issues = json.loads(result_issues.content)
         issues = json.loads(result_issues.content)
-        loop_counter = 0
         issueList = []
-        for issue in issues:
-            if loop_counter == 5:
+        for i in range(0,5):
+            issueList.append(issues[i])
+        access_url = "https://api.github.com/user/repos?access_token=" + self.session.get("access_token") + "&filter=all&sort=pushed&direction=dec"
+        result_repo = urlfetch.fetch(url = access_url,
+                                method=urlfetch.GET,
+                                headers={"Accept": "application/json"},
+                                deadline=10)
+        five_repos = json.loads(result_repo.content)
+        five_last_updated_repo_times = []
+        for i in range(0, 6):
+            five_last_updated_repo_times.append(five_repos[i])
+        repo_time_counter = 0
+        five_most_recent_commits = []
+        repo_count = 1
+        for repo in five_last_updated_repo_times:
+            repo_date = datetime.strptime(five_last_updated_repo_times[repo_count].get('pushed_at')[0:18],"%Y-%m-%dT%H:%M:%S")
+            repo_count += 1
+            if len(five_most_recent_commits) == 5:
                 break
-            issueList.append(issue)
-            loop_counter +=1
+            full_name = repo.get('full_name')
+            access_url = "https://api.github.com/repos/" + full_name + "/commits?access_token=" + self.session.get("access_token")
+            result_commit = urlfetch.fetch(url = access_url,
+                                method=urlfetch.GET,
+                                headers={"Accept": "application/json"},
+                                deadline=10)
+            five_commits = json.loads(result_commit.content)
+            for com_push in five_commits:
+                if len(five_most_recent_commits) == 5:
+                    break
+                commit = com_push.get('commit')
+                author = commit.get('author')
+                time = datetime.strptime(author.get('date')[0:18], "%Y-%m-%dT%H:%M:%S")
+                if time >= repo_date:
+                    five_most_recent_commits.append(com_push)
+                else:
+                    repo_time_counter += 1
+                    break
+        print(len(five_most_recent_commits))
         template = env.get_template('index.html')
-        self.response.write(template.render(name = "Dashboard", issues=issueList, token = self.session.get("access_token"), user = User.query(User.key == self.session.get("user")).get()))
+        self.response.write(template.render(name = "Dashboard", issues=issueList, commits=five_most_recent_commits, token = self.session.get("access_token"), user = User.query(User.key == self.session.get("user")).get()))
 
 
 class Splash(BaseHandler):
