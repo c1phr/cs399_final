baf880061ee07c89c6f02a9bfea6ee1b36b70ff9
Duke Ayers <Dfa24@nau.edu>
2015-04-25 15:02:56 -0700

    Add/Delete/Update Requirements
    Basics for the functioning of the above events are now put into place.

--- app/handlers/requirements.py
+++ app/handlers/requirements.py
@@ -15,25 +15,33 @@ class RequirementsDashboard(BaseHandler):
     def get(self, project_id):
         template = env.get_template("requirements.html")
         project = Project.query(Project.project_id == int(project_id)).get()
-        requirements = Requirements.query(Requirements.project_id == project.key).fetch()
+        requirements = Requirements.query(Requirements.project_id == project.key, Requirements.active == True).fetch()
         self.response.write(template.render(name = "Requirements",user = BaseHandler.user(self), requirements = requirements, project_id = project_id))
 
     def put(self, project_id):
         project = Project.query(Project.project_id == int(project_id)).get()
         parent = cgi.escape(self.request.get("parent"))
-        requirement_id = ndb.Key(urlsafe=cgi.escape(self.request.get("id")))
         description = cgi.escape(self.request.get("description"))
         title = cgi.escape(self.request.get("title"))
         method = cgi.escape(self.request.get("method"))
         if method == "update":
+            requirement_id = ndb.Key(urlsafe=cgi.escape(self.request.get("id")))
             requirement = requirement_id.get()
             requirement.req_title = title
             requirement.req_desc = description
             if parent != "None":
                 requirement.parent_id = parent
             requirement.put()
+        elif method == "delete":
+            requirement_id = ndb.Key(urlsafe=cgi.escape(self.request.get("id")))
+            requirement = requirement_id.get()
+            requirement.active = False
+            requirement.put()
         else:
-            requirement = Requirements(project_id=project.key, req_title=title, req_desc=description)
+            if parent != "None":
+                requirement = Requirements(project_id=project.key, req_title=title, req_desc=description, parent_id = parent)
+            else:
+                requirement = Requirements(project_id=project.key, req_title=title, req_desc=description, parent_id = None)
             try:
                 requirement.put()
                 self.response.status_int = 200
@@ -42,12 +50,3 @@ class RequirementsDashboard(BaseHandler):
                 self.response.status_int = 500
                 self.response.status_message = traceback.format_exception()
 
-    def delete(self, requirement_id):
-        requirement_key = ndb.Key(urlsafe=cgi.escape(requirement_id))
-        try:
-            requirement_key.delete()
-            self.response.status_int = 200
-            self.response.status_message = "Requirement Deleted Successfully"
-        except:
-            self.response.status_int = 500
-            self.response.status_message = traceback.format_exception()
