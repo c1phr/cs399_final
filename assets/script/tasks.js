6fa9fcccb76e8fcdde173b42599105e95de0f01e
Duke Ayers <Dfa24@nau.edu>
2015-04-27 19:29:49 -0700

    Add New Tasks - Complete
    Ability to add the new tasks is now complete.

--- assets/script/tasks.js
+++ assets/script/tasks.js
@@ -41,18 +41,16 @@ function addTask(element){
         url: "/tasks/"+$this.attr('data-requirement'),
         data: {
             requirement: $this.attr('data-requirement'),
-            title: $this.siblings('input').val(),
-            description: $this.siblings('textarea').text(),
-            assignee: $this.siblings('#assigneeUpdate').val(),
-            open: $this.siblings('#statusUpdate').val(),
-            task_key: $this.attr('data-task-key')
+            title: $('#addTaskTitle').val(),
+            description: $('#addTaskDesc').val(),
+            assignee: $('#assigneeNew').val(),
+            open: "Open"
         }
     })
         .success(function(data){
-            editMode($this);
-            $parent = $this.closest('.col-xs-4');
-            $parent.find('h3').text($this.siblings('input').val());
-            $parent.find('h4').text($this.siblings('textarea').text());
-            $parent.find('.user-assigned').text($this.siblings('#assigneeUpdate').children(':selected').text());
+            $('#taskModal').modal('hide');
+             $('#addTaskTitle').val("");
+            $('#addTaskDesc').val("");
+            $('#assigneeNew').val("");
         })
 }
