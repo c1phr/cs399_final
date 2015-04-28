a1347a4daf362d37964558ee04e22277d581544e
Duke Ayers <dfa24@nau.edu>
2015-04-27 19:07:39 -0700

    Add New Tasks
    Ability to add new tasks

--- assets/script/tasks.js
+++ assets/script/tasks.js
@@ -13,7 +13,29 @@ function changeText(element) {
 
 function updateTask(element){
     $this = $(element);
-    console.log($this.siblings('#assigneeUpdate').val())
+    $.ajax({
+        method: "PUT",
+        url: "/tasks/"+$this.attr('data-requirement'),
+        data: {
+            requirement: $this.attr('data-requirement'),
+            title: $this.siblings('input').val(),
+            description: $this.siblings('textarea').text(),
+            assignee: $this.siblings('#assigneeUpdate').val(),
+            open: $this.siblings('#statusUpdate').val(),
+            task_key: $this.attr('data-task-key')
+        }
+    })
+        .success(function(data){
+            editMode($this);
+            $parent = $this.closest('.col-xs-4');
+            $parent.find('h3').text($this.siblings('input').val());
+            $parent.find('h4').text($this.siblings('textarea').text());
+            $parent.find('.user-assigned').text($this.siblings('#assigneeUpdate').children(':selected').text());
+        })
+}
+
+function addTask(element){
+    $this = $(element);
     $.ajax({
         method: "PUT",
         url: "/tasks/"+$this.attr('data-requirement'),
